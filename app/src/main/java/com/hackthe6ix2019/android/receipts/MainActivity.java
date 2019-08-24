2e43206ed19259fc8574d804ee1990d39c276ba9
Alice <34670205+alice-cai@users.noreply.github.com>
2019-08-24 17:00:07 -0400

    update aws code - does not work

--- app/src/main/java/com/hackthe6ix2019/android/receipts/MainActivity.java
+++ app/src/main/java/com/hackthe6ix2019/android/receipts/MainActivity.java
@@ -1,15 +1,28 @@
 package com.hackthe6ix2019.android.receipts;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.TextView;
 import android.support.v4.app.Fragment;
+import android.widget.Toast;
+
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.textract.AmazonTextract;
+import com.amazonaws.services.textract.AmazonTextractClient;
+import com.amazonaws.services.textract.model.DetectDocumentTextRequest;
+import com.amazonaws.services.textract.model.DetectDocumentTextResult;
+import com.amazonaws.services.textract.model.Document;
+
+import java.io.ByteArrayOutputStream;
+import java.nio.ByteBuffer;
 
 public class MainActivity extends AppCompatActivity implements BudgetFragment.OnFragmentInteractionListener {
 
@@ -63,6 +76,38 @@ public class MainActivity extends AppCompatActivity implements BudgetFragment.On
             return true;
         }
     };
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == Image_Capture_Code) {
+            if (resultCode == RESULT_OK) {
+                Bitmap bp = (Bitmap) data.getExtras().get("data");
+                // convert to byte array
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                bp.compress(Bitmap.CompressFormat.PNG, 100, stream);
+                byte[] byteArray = stream.toByteArray();
+                //Toast.makeText(this, byteArray + "", Toast.LENGTH_LONG).show();
+                Log.d("main activity","DATA EXTRAS: " + data.getExtras());
+
+                // aws stuff
+                //AmazonRekognition rekognitionClient = new AmazonRekognitionClient(new BasicAWSCredentials("", ""));
+
+                // TODO: remove later
+                BasicAWSCredentials credentials = new BasicAWSCredentials("AKIASMTWCNOQRPPVZXFP", "zbRVfIYS2Y+6a92/jUtn4+B8fUUi6cgjwuZ5F/Wu");
+
+                AmazonTextractClient amazonTextractClient = new AmazonTextractClient(credentials);
+                ByteBuffer sourceImageBytes = ByteBuffer.wrap(byteArray);
+                Document source = new Document().withBytes(sourceImageBytes);
+                DetectDocumentTextRequest documentTextRequest = new DetectDocumentTextRequest().withDocument(source);
+                DetectDocumentTextResult documentTextResult = amazonTextractClient.detectDocumentText(documentTextRequest);
+                System.out.println(documentTextResult);
+                Log.d("main activity","RESULT: " + documentTextResult);
+                Toast.makeText(this, documentTextResult + "", Toast.LENGTH_LONG).show();
+            } else if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
 
     public void onFragmentInteraction(Uri uri) {
         // TODO: what
